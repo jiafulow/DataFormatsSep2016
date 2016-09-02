4e90f6b315070f4c3b745b3857ee4d5835c5266c
Jia Fu Low <jia.fu.low@cern.ch>
2016-09-02 08:20:58 -0400

    Add BuildFile.xml and src/classes.h src/classes_def.xml

---
+++ L1TMuon/src/classes.h
@@ -0,0 +1,24 @@
+#include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/Common/interface/RefToBase.h"
+
+#include "DataFormatsSep2016/L1TMuon/interface/EMTFTrack.h"
+#include "DataFormatsSep2016/L1TMuon/interface/EMTFHit.h"
+#include "DataFormatsSep2016/L1TMuon/interface/EMTFTrackExtra.h"
+#include "DataFormatsSep2016/L1TMuon/interface/EMTFHitExtra.h"
+
+namespace {
+  struct dictionary {
+    L1TMuonEndCap::EMTFTrackCollection emtfTrack;
+    edm::Wrapper<L1TMuonEndCap::EMTFTrackCollection> emtfTrackWrap;
+
+    L1TMuonEndCap::EMTFHitCollection emtfHit;
+    edm::Wrapper<L1TMuonEndCap::EMTFHitCollection> emtfHitWrap;
+
+    L1TMuonEndCap::EMTFTrackExtraCollection emtfTrackExtra;
+    edm::Wrapper<L1TMuonEndCap::EMTFTrackExtraCollection> emtfTrackExtraWrap;
+
+    L1TMuonEndCap::EMTFHitExtraCollection emtfHitExtra;
+    edm::Wrapper<L1TMuonEndCap::EMTFHitExtraCollection> emtfHitExtraWrap;
+  };
+}
+
