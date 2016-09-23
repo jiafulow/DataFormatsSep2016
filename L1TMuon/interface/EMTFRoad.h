5600b909da952f107567adef3f66c5da386ededf
Jia Fu Low <jia.fu.low@cern.ch>
2016-09-23 16:32:17 -0400

    Add empty EMTFRoad

---
+++ L1TMuon/interface/EMTFRoad.h
@@ -0,0 +1,19 @@
+#ifndef __l1t_EMTFRoad_h__
+#define __l1t_EMTFRoad_h__
+
+#include <cstdint>
+#include <vector>
+
+namespace L1TMuonEndCap {
+
+  class EMTFRoad {
+  
+  }; // End of class EMTFRoad
+  
+  // Define a vector of EMTFRoad
+  typedef std::vector<EMTFRoad> EMTFRoadCollection;
+
+} // End of namespace l1t
+
+#endif /* define __l1t_EMTFRoad_h__ */
+
