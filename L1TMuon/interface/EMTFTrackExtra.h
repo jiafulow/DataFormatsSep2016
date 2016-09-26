6814a7223f35fddeb588d2e506b00132b2ca3495
Jia Fu Low <jia.fu.low@cern.ch>
2016-09-26 16:11:59 -0400

    Update EMTFTrackExtra

--- L1TMuon/interface/EMTFTrackExtra.h
+++ L1TMuon/interface/EMTFTrackExtra.h
@@ -16,10 +16,10 @@ namespace L1TMuonEndCap {
 
     EMTFTrackExtra() : endcap(0), sector(0), bx(0), first_bx(0), second_bx(0),
                        rank(0), winner(0), mode(0), mode_inv(0),
+                       phi_int(0), theta_int(0),
                        ptlut_address(0),
                        pt(0.), pt_xml(0.),
-                       pt_int(0), phi_int(0), theta_int(0),
-                       gmt_phi(0), gmt_eta(0), gmt_quality(0), gmt_charge(0),
+                       gmt_pt(0), gmt_phi(0), gmt_eta(0), gmt_quality(0), gmt_charge(0),
                        ptlut_data(),
                        xroad(),
                        num_xhits(0),
@@ -43,6 +43,10 @@ namespace L1TMuonEndCap {
     uint16_t mode;
     uint16_t mode_inv;
 
+    // phi, theta
+    uint16_t phi_int;
+    uint16_t theta_int;
+
     // pT LUT address
     uint64_t ptlut_address;
 
@@ -50,16 +54,12 @@ namespace L1TMuonEndCap {
     float    pt;
     float    pt_xml;
 
-    // Momentum
-    uint32_t pt_int;
-    uint32_t phi_int;
-    uint32_t theta_int;
-
     // GMT
-    int gmt_phi;
-    int gmt_eta;
-    int gmt_quality;
-    int gmt_charge;
+    int32_t gmt_pt;
+    int32_t gmt_phi;
+    int32_t gmt_eta;
+    int32_t gmt_quality;
+    int32_t gmt_charge;
 
     // pT LUT data
     EMTFPtLUTData ptlut_data;
@@ -68,7 +68,7 @@ namespace L1TMuonEndCap {
     EMTFRoadExtra xroad;
 
     // Number of hits
-    uint32_t num_xhits;
+    uint16_t num_xhits;
 
     // Hits
     std::vector<EMTFHitExtra> xhits;
