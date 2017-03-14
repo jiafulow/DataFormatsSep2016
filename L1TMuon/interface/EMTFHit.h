a0f0d9e215b403d1855aa06e7717c7bdf1473970
Jia Fu Low <jia.fu.low@cern.ch>
2017-03-14 19:17:24 -0400

    Update interface/EMTFHit.h

--- L1TMuon/interface/EMTFHit.h
+++ L1TMuon/interface/EMTFHit.h
@@ -1,25 +1,39 @@
-#ifndef __L1TMuon_EMTFHitExtra_h__
-#define __L1TMuon_EMTFHitExtra_h__
+// Class for input trigger primitives to EMTF - AWB 04.01.16
+// Based on L1Trigger/L1TMuon/interface/deprecate/MuonTriggerPrimitive.h
+// In particular, see struct CSCData
 
-#include <cstdint>
+#ifndef __l1t_EMTFHit_h__
+#define __l1t_EMTFHit_h__
+
 #include <vector>
+#include <boost/cstdint.hpp> 
+#include <cmath>
+#include <iostream>
 
-namespace L1TMuonEndCap {
+#include "DataFormats/MuonDetId/interface/CSCDetId.h"
+#include "DataFormats/MuonDetId/interface/RPCDetId.h"
+#include "DataFormats/CSCDigi/interface/CSCCorrelatedLCTDigi.h"
+#include "DataFormats/RPCDigi/interface/RPCDigi.h"
+#include "DataFormats/L1TMuon/interface/EMTF/ME.h"
 
-  struct EMTFHitExtra {
+namespace l1t {
+  
+  class EMTFHit {
+  public:
+    
+    EMTFHit() :
+        endcap(0), station(0), ring(0), roll(0), chamber(0), sector(0), subsector(0), csc_ID(0), cscn_ID(0),
+        bx(0), subsystem(0),
+        pc_sector(0), pc_station(0), pc_chamber(0), pc_segment(0),
+        valid(0), strip(0), strip_low(0), strip_hi(0), wire(0), quality(0), pattern(0), bend(0),
+        phi_fp(0), theta_fp(0), phzvl(0), ph_hit(0), zone_hit(0), zone_code(0),
+        fs_segment(0), fs_zone_code(0), bt_station(0), bt_segment(0),
+        phi_loc_deg(0), phi_glob_deg(0), theta_deg(0), eta(0)
+        {};
 
-    EMTFHitExtra() : detId(0),
-                     endcap(0), station(0), ring(0), roll(0), chamber(0), sector(0), subsector(0), csc_ID(0), cscn_ID(0),
-                     bx(0), subsystem(0),
-                     pc_sector(0), pc_station(0), pc_chamber(0), pc_segment(0),
-                     valid(0), strip(0), strip_low(0), strip_hi(0), wire(0), quality(0), pattern(0), bend(0),
-                     phi_fp(0), theta_fp(0), phzvl(0), ph_hit(0), zone_hit(0), zone_code(0),
-                     fs_segment(0), fs_zone_code(0), bt_station(0), bt_segment(0),
-                     phi_loc_deg(0), phi_glob_deg(0), theta_deg(0), eta(0)
-                     {}
+    virtual ~EMTFHit() {};
 
-    // DetId
-    uint32_t detId;
+  private:
 
     int16_t endcap;
     int16_t station;
@@ -75,11 +89,11 @@ namespace L1TMuonEndCap {
     float theta_deg;
     float eta;
 
-  };  // class EMTFHitExtra
+  }; // End of class EMTFHit
+  
+  // Define a vector of EMTFHit
+  typedef std::vector<EMTFHit> EMTFHitCollection;
+  
+} // End of namespace l1t
 
-  // Define a vector of EMTFHitExtra
-  typedef std::vector<EMTFHitExtra> EMTFHitExtraCollection;
-
-}
-
-#endif
+#endif /* define __l1t_EMTFHit_h__ */
