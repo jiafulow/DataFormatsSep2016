4552b4b884c10dc4149002146e7bf4a191743c2d
Jia Fu Low <jia.fu.low@cern.ch>
2017-03-14 20:24:17 -0400

    Update interface/EMTFTrack.h [2]
    - Add EMTFPtLUT member struct
      - Add variables: address, mode, theta, eta

--- L1TMuon/interface/EMTFTrack.h
+++ L1TMuon/interface/EMTFTrack.h
@@ -13,6 +13,24 @@
 
 
 namespace l1t {
+
+  struct EMTFPtLUT {
+    unsigned long address;
+    uint16_t mode;
+    uint16_t theta;
+    uint16_t eta;
+    uint16_t delta_ph  [6]; // index: 0=12, 1=13, 2=14, 3=23, 4=24, 5=34
+    uint16_t delta_th  [6]; // ^
+    uint16_t sign_ph   [6]; // ^
+    uint16_t sign_th   [6]; // ^
+    uint16_t cpattern  [4]; // index: 0=ME1, 1=ME2, 2=ME3, 3=ME4
+    uint16_t fr        [4]; // ^
+    uint16_t bt_vi     [5]; // index: 0=ME1sub1, 1=ME1sub2, 2=ME2, 3=ME3, 4=ME4
+    uint16_t bt_hi     [5]; // ^
+    uint16_t bt_ci     [5]; // ^
+    uint16_t bt_si     [5]; // ^
+  };
+
   class EMTFTrack {
   public:
 
