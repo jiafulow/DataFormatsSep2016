c03a4bee0a0138d647febb12245404fff4d4af03
Jia Fu Low <jia.fu.low@cern.ch>
2017-03-14 20:42:59 -0400

    Update interface/EMTFTrack.h [3]
    - Add variables: sector_idx, theta, eta, has_neighbor, all_neighbor, numHits
    - Remove variables: gmt_pt, gmt_phi, gmt_eta, gmt_quality, gmt_charge, gmt_charge_valid
    - Rename variables: xmlpt -> pt_XML, theta_int -> theta_fp, phi_int -> phi_fp, xhits -> _Hits, ptlut_data -> _PtLUT
    - Change types: int16_t -> int, uint16_t -> int, EMTFPtLUTData -> EMTFPtLUT vector<EMTFHitExtra> -> EMTFHitCollection
    - Change defaults: 0 -> -99

--- L1TMuon/interface/EMTFTrack.h
+++ L1TMuon/interface/EMTFTrack.h
@@ -15,7 +15,7 @@
 namespace l1t {
 
   struct EMTFPtLUT {
-    unsigned long address;
+    uint64_t address;
     uint16_t mode;
     uint16_t theta;
     uint16_t eta;
@@ -31,69 +31,53 @@ namespace l1t {
     uint16_t bt_si     [5]; // ^
   };
 
+
   class EMTFTrack {
   public:
 
-    EMTFTrackExtra() : endcap(0), sector(0), bx(0), first_bx(0), second_bx(0),
-                       zone(0), ph_num(0), ph_q(0),
-                       rank(0), winner(0), track_num(0), mode(0), mode_inv(0),
-                       phi_int(0), theta_int(0),
-                       ptlut_address(0),
-                       gmt_pt(0), gmt_phi(0), gmt_eta(0), gmt_quality(0), gmt_charge(0), gmt_charge_valid(0),
-                       pt(0.), xmlpt(0.),
-                       ptlut_data(),
-                       xhits()
-                       {}
+    EMTFTrack() : 
+        endcap(-99), sector(-99), sector_idx(-99), mode(-99), mode_inv(-99),
+        rank(-99), winner(-99), charge(-99), bx(-99), first_bx(-99), second_bx(-99),
+        pt(-99), pt_XML(-99), zone(-99), ph_num(-99), ph_q(-99),
+        theta_fp(-99), theta(-99), eta(-99), phi_fp(-99), phi_loc(-99), phi_glob(-99),
+        track_num(-99), has_neighbor(-99), all_neighbor(-99), numHits(-99)
+        {};
 
-    // DetId
-    int16_t endcap;
-    int16_t sector;
+    explicit ~EMTFTrack() {};
 
-    // BX
-    int16_t bx;
-    int16_t first_bx;
-    int16_t second_bx;
 
-    // Zone
-    uint16_t zone;
+  private:
 
-    // Phi pattern
-    uint16_t ph_num;
-    uint16_t ph_q;
+    EMTFPtLUT _PtLUT;
 
-    // Rank
-    uint16_t rank;
-    uint16_t winner;  // 0: first winner, 1: second winner, ...
-    uint16_t track_num;  // the final position after removing invalid tracks and ghosts
+    EMTFHitCollection _Hits;
 
-    // Mode
-    uint16_t mode;
-    uint16_t mode_inv;
-
-    // phi, theta
-    uint16_t phi_int;
-    uint16_t theta_int;
-
-    // pT LUT address
-    uint64_t ptlut_address;
-
-    // GMT
-    int32_t gmt_pt;
-    int32_t gmt_phi;
-    int32_t gmt_eta;
-    int32_t gmt_quality;
-    int32_t gmt_charge;
-    int32_t gmt_charge_valid;
-
-    // pT from XML
-    float    pt;
-    float    xmlpt;
-
-    // pT LUT data
-    EMTFPtLUTData ptlut_data;
-
-    // Hits
-    std::vector<EMTFHitExtra> xhits;
+    int endcap;
+    int sector;
+    int sector_idx;
+    int mode;
+    int mode_inv;
+    int rank;
+    int winner;  // 0: first winner, 1: second winner, ...
+    int charge;
+    int bx;
+    int first_bx;
+    int second_bx;
+    float pt;
+    float pt_XML;
+    int zone;
+    int ph_num;
+    int ph_q;
+    int theta_fp;
+    float theta;
+    float eta;
+    int phi_fp;
+    float phi_loc;
+    float phi_glob;
+    int track_num;  // the final position after removing invalid tracks and ghosts
+    int has_neighbor;
+    int all_neighbor;
+    int numHits;
 
   }; // End of class EMTFTrack
   
