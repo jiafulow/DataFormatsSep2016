4118e5bc6a03f18530157316583dc5c81e215597
Jia Fu Low <jia.fu.low@cern.ch>
2017-03-14 21:04:07 -0400

    Update src/EMTFTrack.cc

--- L1TMuon/src/EMTFTrack.cc
+++ L1TMuon/src/EMTFTrack.cc
@@ -1,6 +1,4 @@
-#include "DataFormatsSep2016/L1TMuon/interface/EMTFTrackExtra.h"
+#include "DataFormats/L1TMuon/interface/EMTFTrack.h"
 
-#include <cassert>
-#include <type_traits>
-//static_assert(std::is_trivially_copyable<L1TMuonEndCap::EMTFTrackExtra>::value, "EMTFTrackExtra is not trivially copyable");  // is false
-
+namespace l1t {
+} // End namespace l1t
